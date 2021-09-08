dbfcc2f270576e8d9421b446ae7b6f9aa8f63725
Sungoh Park <spark449062@gmail.com>
2021-09-08 15:11:07 -0600

    commit tree f476ba1ba5089586421c1b9febaa14a42a80a5ba

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, sex) VALUES ("Mr. Chocolate", "M");
+INSERT INTO bears (name, sex) VALUES ("Rowdy", "F");
+INSERT INTO bears (name, sex) VALUES ("Tabitha", "M");
+INSERT INTO bears (name, sex) VALUES ("Sergeant Brown", "F");
+INSERT INTO bears (name, sex) VALUES ("Melissa", "M");
+INSERT INTO bears (name, sex) VALUES ("Grinch", "F");
+INSERT INTO bears (name, sex) VALUES ("Wendy", "M");
+INSERT INTO bears (sex) VALUES ("F");
+
